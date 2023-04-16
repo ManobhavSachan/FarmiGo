9d9d188a6d32ccd87be06ed014453883876a3ec4
Ambesh <ambeshgaunker@gmail.com>
2023-04-16 21:23:16 +0530

    newcommit

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import blog1 from "../images/blog-1.jpg"
+import blog1 from "../images/blog-1.jpg";
 const BlogCard = () => {
   return (
     <div className="blog-card">
@@ -11,8 +11,9 @@ const BlogCard = () => {
         <p className="date">15 April 2023</p>
         <h5 className="title">Problems faced by farmers in existing market</h5>
         <p className="desc">
-        Firstly ,the mandis were about 30 to 50 km far away from the farmers ,and more than 80 percent of the farmers in
-our country are still small farmers .So....
+          Firstly ,the mandis were about 30 to 50 km far away from the farmers
+          ,and more than 80 percent of the farmers in our country are still
+          small farmers .So....
         </p>
         <Link to="/blog/:id" className="button">
           Read More
